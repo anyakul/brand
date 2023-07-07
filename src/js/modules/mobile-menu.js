d07a96e6759fb3d5c0e938c1e667ff08674118ef
anyakulikova111@ <anyakulikova111@gmail.com>
2023-07-07 14:56:33 +0300

    lesson1

--- src/js/modules/mobile-menu.js
+++
@@ -1,8 +0,0 @@
-export function mobileMenu() {
-  let menuButton = document.querySelector(".header-button-burger")
-  let menu = document.querySelector(".header")
-
-  menuButton.addEventListener('click', function () {
-    menu.classList.toggle('header-active')
-  })
-}
