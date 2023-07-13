ca7a6205c98002287f7e68b3f2dc9d83ea924828
anyakulikova111@ <anyakulikova111@gmail.com>
2023-07-13 15:01:28 +0300

    lesson1

--- src/js/modules/slider.js
+++ src/js/modules/slider.js
@@ -15,4 +15,43 @@ export function slider() {
       clickable: true,
     },
   });
+
+  const productsSwiper = new Swiper('.products', {
+    slidesPerView: 1,
+    observer: true,
+    observeParents: true,
+    navigation: {
+      nextEl: '.pagination__button-next',
+      prevEl: '.pagination__button-prev',
+    },
+    pagination: {
+      el: '.pagination__list',
+      clickable: true,
+      type: 'custom',
+      dynamicBullets: true,
+      dynamicMainBullets: 5,
+      dynamicBullets: true,
+      dynamicMainBullets: 5,
+      renderCustom: function (swiper, current, total) {
+        var paginationHtml = '';
+        for (var i = 1; i <= total; i++) {
+          if (i == current) {
+            paginationHtml += `<li class="pagination__item pagination__item--active">${i}</li>`
+          } else if (i > current - 6 && i < current + 6) {
+            paginationHtml += `<li class="pagination__item">${i}</li>`
+          } else if (i == total - 1) {
+            paginationHtml += `<li class="pagination__item pagination__item--dot">...</li>`
+          } else if (i == 1) {
+            paginationHtml += `<li class="pagination__item pagination__item--dot">...</li>`
+          } else if (i == total) {
+            paginationHtml += `<li class="pagination__item">${i}</li>`
+          }
+        }
+        return paginationHtml
+      },
+      hashNavigation: {
+        watchState: true,
+      },
+    }
+  });
 }
