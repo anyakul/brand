a521a4d327578cac8a9abd6b0b716537184a7d97
anyakulikova111@ <anyakulikova111@gmail.com>
2023-07-13 13:35:21 +0300

    lesson1

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,9 @@
 import * as slider from './modules/slider.js';
 import * as form from './modules/form.js';
+import * as header from './modules/header.js';
 import Swiper, { Navigation, Pagination } from 'swiper';
 
+header.header();
 slider.slider();
 form.form();
 
--- src/js/modules/form.js
+++ src/js/modules/form.js
@@ -1,12 +1,32 @@
 export function form() {
+  let productForm = document.querySelector(".product-form");
+  let filterButton = document.querySelector(".product-form__filter-button");
+  let filterList = document.querySelector(".product-form__filter");
+  let filterSubListButton = document.querySelectorAll(".product-form__filter-item-button");
+  let filterSubList = document.querySelectorAll(".product-form__filter-item-sublist");
   let selectButtons = document.querySelectorAll(".product-form__item-button");
   let select = document.querySelectorAll(".product-form__sublist");
   let subitems = document.querySelectorAll(".product-form__subitem");
 
-  for (let i = 0; i < selectButtons.length; i++) {
-    selectButtons[i].addEventListener('click', function (e) {
+  if (productForm != null) {
+    for (let i = 0; i < selectButtons.length; i++) {
+      selectButtons[i].addEventListener('click', function (e) {
+        e.preventDefault();
+        select[i].classList.toggle('product-form__sublist--active');
+      })
+    }
+
+    filterButton.addEventListener('click', function (e) {
       e.preventDefault();
-      select[i].classList.toggle('product-form__sublist--active');
+      filterList.classList.add('product-form__filter--active');
     })
+
+    for (let i = 0; i < filterSubListButton.length; i++) {
+      filterSubListButton[i].addEventListener('click', function (e) {
+        e.preventDefault();
+        filterSubList[i].classList.toggle('product-form__sublist--active');
+        filterSubListButton[i].classList.toggle('product-form__filter-item-button--active');
+      })
+    }
   }
 }
